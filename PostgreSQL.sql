f11ac9a7be0dc1486f8f2df8bca6637f558bc69a
Rpanguana <filipe.panguana@outlook.com>
2019-12-03 12:47:42 +0200

    added all necessary changes

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -1,5 +1,6 @@
 PART 1: CREATING A DATABASE
 
+
 1.
  docker run --name umuzi -e POSTGRES_PASSWORD=Password -d postgres
 
@@ -8,9 +9,9 @@ PART 1: CREATING A DATABASE
  CREATE DATABASE umuzi;
 
 
-2. && 3 && 4.
+2. && 3 && 4. 
  \c umuzi;
- CREATE TABLE customers(
+ CREATE TABLE customers(    
  CustomerID SERIAL PRIMARY KEY,
  FirstName VARCHAR(50),
  LastName VARCHAR(50),
@@ -48,37 +49,49 @@ PART 1: CREATING A DATABASE
  DateShipped TIMESTAMP,
  Status VARCHAR(20));
 
-
--- 4.
---  CREATE TABLE Payments(CustomerID integer REFERENCES Customers(CustomerID), PaymentID SERIAL PRIMARY KEY, PaymentDate TIMESTAMP, Amount DECIMAL);
-
-
 5.
+
+--CUSTOMERS TABLE
  INSERT INTO customers values(1, 'John', 'Hilbert', 'Male', '284 chaucer st', '084789657', 'john@gmail.com', 'Johannesburg', 'South Africa');
  INSERT INTO customers values(2, 'Thando', 'Sithole', 'Female', '240 Sect 1', '0794445584', 'thando@gmail.com', 'Cape Town', 'South Africa');
  INSERT INTO customers values(3, 'Leon', 'Glen', 'Male', '81 Everton Rd,Gillits', '0820832830', 'Leon@gmail.com', 'Durban', 'South Africa');
  INSERT INTO customers values(4, 'Charl', 'Muller', 'Male', '290A Dorset Ecke', '+44856872553', 'Charl.muller@yahoo.com', 'Berlin', 'Germany');
  INSERT INTO customers values(5, 'Julia', 'Stein', 'Female', '2 Wernerring', '+448672445058', 'Js234@yahoo.com', 'Frankfurt', 'Germany');
 
+--EMPLOYEES TABLE
  INSERT INTO employees values(1, 'Kani', 'Matthew', 'mat@gmail.com', 'Manager');
  INSERT INTO employees values(2, 'Lesly', 'Cronje', 'LesC@gmail.com', 'Clerk');
  INSERT INTO employees values(3, 'Gideon', 'Maduku', 'm@gmail.com', 'Accountant');
 
+--ORDERS TABLE
  INSERT INTO Orders values(1, 1, 1, 2, 05-09-2018, DEFAULT, 'Not Shipped');
  INSERT INTO Orders values(2, 1, 2, 2, 04-09-2018, 03-09-2018, 'Shipped');
  INSERT INTO Orders values(3, 3, 3, 3, 06-09-2018, DEFAULT, 'Not Shipped');
  
+--PAYMENTS TABLE
  INSERT INTO Payments values(1, 1, 01-09-2018, 150.75);
  INSERT INTO Payments values(5, 2, 03-09-2018, 150.75);
  INSERT INTO Payments values(4, 3, 03-09-2018, 700.60);
 
-INSERT INTO Products values(1, "Harley Davidson Chopper", "This replica features working kickstand, front suspension, gear-shift lever", 150.75);
-INSERT INTO Products values(2, "Classic Car", "Turnable front wheels, steering function", 550.75);
-INSERT INTO Products values(3, "Sports car", "Turnable front wheels, steering function", 700.60);
+--PRODUCTS TABLE
+ INSERT INTO Products values(1, "Harley Davidson Chopper", "This replica features working kickstand, front suspension, gear-shift lever", 150.75);
+ INSERT INTO Products values(2, "Classic Car", "Turnable front wheels, steering function", 550.75);
+ INSERT INTO Products values(3, "Sports car", "Turnable front wheels, steering function", 700.60);
 
-
-   
  SELECT * FROM Customers;
+
+6. -- the tables created control each customers orders and purchases and also stores customers contact and residential details.
+
+-- CUSTOMERS-table includes the customers personal, contact and residential details.
+-- this table would be the primary table
+
+-- EMPLOYEES-table stores all the employees details and is referenced in the ORDERS-table
+
+-- ORDERS-table has the infromation of the orders processed, employees who processed the orders referenced from employees table.
+
+-- PAYMENTS-table keeps the payments made to the company from the customers. CustomerID referenced from CUSTOMERS-table. paymentsID referenced from the payments table
+
+-- PRODUCTS-table has the products being sold and their prices
 
 
 
